45a11991f3d5438ecc9645aa1d51aa141274afc9
NAVSAY <syeda.ambreen.yawar@nav.no>
2025-03-24 13:23:09 +0000

    requirements_airflow.txt fil og endring i sql fil

--- dags/esyfovarsel_dag.py
+++ dags/esyfovarsel_dag.py
@@ -16,7 +16,7 @@ with DAG('esyfovarsel_dag', start_date = days_ago(1) , schedule_interval="0 4 * 
                             "id": "f68dc288-8f02-4111-9ff9-39d4cc8d6168",
                             "token": Variable.get("TEAMSYKEFRAVR_TOKEN"),
                         },
-                         requirements_path="requirements.txt",
+                         requirements_path="requirements_airflow.txt",
                          use_uv_pip_install=True,
                          resources=k8s.V1ResourceRequirements(
                             requests={"ephemeral-storage": "1Gi"},
--- esyfovarsel.sql
+++ esyfovarsel.sql
@@ -1,4 +1,4 @@
 /*esyfovarsel_alt*/
-SELECT * FROM EXTERNAL_QUERY("team-esyfo-prod-bbe6.europe-north1.esyfovarsel", "SELECT aktor_id, type, utsendt_tidspunkt, kanal FROM utsendt_varsel;");
+SELECT * FROM EXTERNAL_QUERY("team-esyfo-prod-bbe6.europe-north1.esyfovarsel", "SELECT type, utsendt_tidspunkt, kanal FROM utsendt_varsel;");
 
 
