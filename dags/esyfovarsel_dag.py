374e5d4312b76331b2a8606cfa2f38f5d1a592da
larsksut <lars.sutterud@nav.no>
2025-03-25 09:06:16 +0000

    fikse sti

--- dags/esyfovarsel_dag.py
+++ dags/esyfovarsel_dag.py
@@ -11,7 +11,7 @@ with DAG('esyfovarsel_dag', start_date = days_ago(1) , schedule_interval="0 4 * 
                          name="esyfovarsel_dag",
                          repo="navikt/esyfovarsel_analyse",
                          quarto={
-                            "path": "esyfovarsel_analyse/esyfovarsel.py",
+                            "path": "esyfovarsel.py",
                             "env": "prod",
                             "id": "f68dc288-8f02-4111-9ff9-39d4cc8d6168",
                             "token": Variable.get("TEAMSYKEFRAVR_TOKEN"),
