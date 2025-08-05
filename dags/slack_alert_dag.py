2837d586d83c06d372ed0004980f5aa6c44a03a3
NAVSAY <syeda.ambreen.yawar@nav.no>
2025-08-05 14:03:18 +0000

    test again slack apiposter full test

--- dags/slack_alert_dag.py
+++ dags/slack_alert_dag.py
@@ -262,39 +262,105 @@ import re
 #     feilende_task >> [slack_ved_feil, t_sjekk_send]
 
 
+# import os
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator, ShortCircuitOperator
+# from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+# from airflow.utils.trigger_rule import TriggerRule
+# from datetime import datetime
+# from kubernetes import client as k8s
+
+# def feilende_funksjon(**context):
+#     raise Exception("💥 Test-feil i feilende_task!")
+
+# def sjekk_dummy(**context):
+#     return True
+
+# with DAG(
+#     dag_id='test_dag_slack_api_feil',
+#     start_date=datetime(2025, 8, 1),
+#     schedule_interval=None,
+#     catchup=False,
+#     default_args={"retries": 0},
+# ) as dag:
+
+#     feilende_task = PythonOperator(
+#         task_id='feilende_task',
+#         python_callable=feilende_funksjon,
+#     )
+
+#     slack_ved_feil = SlackAPIPostOperator(
+#         task_id='slack_ved_feil',
+#         slack_conn_id='slack_connection',  # Airflow connection med Slack bot token
+#         channel="#syfortellinger-alert",      # Sett kanal her
+#         text=":rotating_light: *Task `feilende_task` feilet i DAG `test_dag_slack_api_feil`!*",
+#         trigger_rule=TriggerRule.ONE_FAILED,  # Kjør om en foregående task feiler
+#         executor_config={
+#             "pod_override": k8s.V1Pod(
+#                 metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
+#             )
+#         },
+#     )
+
+#     t_sjekk_send = ShortCircuitOperator(
+#         task_id='t_sjekk_send',
+#         python_callable=sjekk_dummy,
+#     )
+
+#     # Hvis feil i feilende_task, send slack alert, ellers kjør sjekk
+#     feilende_task >> [slack_ved_feil, t_sjekk_send]
+
+
+
 import os
+import pandas_gbq
 from airflow import DAG
+from datetime import datetime
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
 from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 from airflow.utils.trigger_rule import TriggerRule
-from datetime import datetime
 from kubernetes import client as k8s
 
-def feilende_funksjon(**context):
-    raise Exception("💥 Test-feil i feilende_task!")
 
-def sjekk_dummy(**context):
-    return True
+def get_n_rows_yesterday(**context):
+    project = 'teamsykefravr-prod-7e29'
+    sql = 'SELECT * FROM EXTERNAL_QUERY("team-esyfo-prod-bbe6.europe-north1.esyfovarsel", "SELECT utsendt_forsok_tidspunkt FROM utsending_varsel_feilet where utsendt_forsok_tidspunkt > CURRENT_DATE - 7 and utsendt_forsok_tidspunkt < CURRENT_DATE;")'
+
+    
+    df = pandas_gbq.read_gbq(sql, project_id=project)
+    count = len(df)
+    context['ti'].xcom_push(key='row_count', value=count)
+    return count
+
+
+def skal_sendes_slack(**context):
+    count = context['ti'].xcom_pull(task_ids='varsel_status', key='row_count')
+    return count > 0  # Kjør videre kun hvis count > 0
+
 
 with DAG(
-    dag_id='test_dag_slack_api_feil',
-    start_date=datetime(2025, 8, 1),
-    schedule_interval=None,
+    dag_id='overvakning',
+    schedule_interval="0 4 * * *",
+    start_date=datetime(2025, 7, 10),
     catchup=False,
-    default_args={"retries": 0},
+    default_args={"retries": 1},
 ) as dag:
 
-    feilende_task = PythonOperator(
-        task_id='feilende_task',
-        python_callable=feilende_funksjon,
+    # Hent antall rader
+    t_feilet_status = PythonOperator(
+        task_id='varsel_status',
+        python_callable=get_n_rows_yesterday,
+        provide_context=True,
     )
 
+    # Slack-varsling hvis varsel_status task feiler
     slack_ved_feil = SlackAPIPostOperator(
-        task_id='slack_ved_feil',
-        slack_conn_id='slack_connection',  # Airflow connection med Slack bot token
-        channel="#syfortellinger-alert",      # Sett kanal her
-        text=":rotating_light: *Task `feilende_task` feilet i DAG `test_dag_slack_api_feil`!*",
-        trigger_rule=TriggerRule.ONE_FAILED,  # Kjør om en foregående task feiler
+        task_id='slack_ved_feil_i_feilet_status',
+        slack_conn_id='slack_connection',
+        channel='#syfortellinger-alert',
+        text=":rotating_light:  *testvarsle:Task `varsel_status` feilet i DAG `overvakning`!*",
+        trigger_rule=TriggerRule.ONE_FAILED,  # Kjør om denne eller noen forrige feiler
         executor_config={
             "pod_override": k8s.V1Pod(
                 metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
@@ -302,10 +368,34 @@ with DAG(
         },
     )
 
+    # Sjekk om det finnes rader og skal varsle videre
     t_sjekk_send = ShortCircuitOperator(
-        task_id='t_sjekk_send',
-        python_callable=sjekk_dummy,
+        task_id='sjekk_send_slack',
+        python_callable=skal_sendes_slack,
+        provide_context=True,
     )
 
-    # Hvis feil i feilende_task, send slack alert, ellers kjør sjekk
-    feilende_task >> [slack_ved_feil, t_sjekk_send]
+    # Slack-varsling dersom rader funnet
+    t_varsling = SlackAPIPostOperator(
+        task_id="varsling",
+        slack_conn_id="slack_connection",
+        channel="#syfortellinger-alert",
+        text=(
+            "testvarsle. "
+            "NB! Nye rader i `esyfovarsel.utsendt_varsel_feilet` i går. "
+            "Antall rader: {{ ti.xcom_pull(task_ids='varsel_status', key='row_count') }}"
+        ),
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
+            )
+        },
+    )
+
+    t_stop = EmptyOperator(task_id="stop_task")
+
+    # Rekkefølge:
+    # Når varsel_status kjører, så på feilmelding send slack_ved_feil,
+    # ellers sjekk om varsling skal sendes for rader
+    t_feilet_status >> [t_sjekk_send, slack_ved_feil]
+    t_sjekk_send >> t_varsling >> t_stop
